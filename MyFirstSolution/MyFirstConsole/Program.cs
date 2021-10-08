1f4dd1430439e5e92a5823b48939dd3ebbe0d256
DESKTOP-JJHS10S\Влад <logisticpro3000@gmail.com>
2021-10-08 20:00:53 +0300

    Task 2.1 and Task 2.2

---
+++ MyFirstSolution/MyFirstConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            Console.WriteLine(number);
+
+            Console.ReadKey();
+        }
+    }
+}
