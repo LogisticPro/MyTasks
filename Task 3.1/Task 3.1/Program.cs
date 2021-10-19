6886c93854d4094019f9b798e5fc87e02da461b7
DESKTOP-JJHS10S\Влад <logisticpro3000@gmail.com>
2021-10-19 11:56:54 +0300

    task3

---
+++ Task 3.1/Task 3.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение радиуса");
+
+            string radius = Console.ReadLine();
+            double r;
+
+            while (Double.TryParse(radius, out r))
+            {              
+                Console.WriteLine("Вы ввели неверное значение, повторите");
+                radius = Console.ReadLine();
+            }
+
+            Console.WriteLine("Введите значение образующей");
+
+            string generatrix = Console.ReadLine();
+            double l;
+
+            while (!Double.TryParse(generatrix, out l))
+            {
+                Console.WriteLine("Вы ввели неверное значение, повторите");
+                generatrix = Console.ReadLine();
+            }
+
+            double s = Math.PI * r * (r + l);
+
+            Console.WriteLine("Площадь круглого конуса равна: {0}", s);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 3.1/Task 3.2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int x = 14, y = 1, z = 5;
+
+            for (int i = 0; i < 5; i++)
+            {             
+                x += y - x++ * z;
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            x = 14; y = 1; z = 5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                z = --x - y * 5;
+                Console.WriteLine(z);
+            }
+            Console.WriteLine();
+
+            double x = 14, y = 1, z = 5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                y /= x + 5 % z;
+                Console.WriteLine(y);
+            }
+            Console.WriteLine();
+
+            double x = 14, y = 1, z = 5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                z = x++ + y * 5;
+                Console.WriteLine(z);
+            }
+            Console.WriteLine();*/
+
+            double x = 14, y = 1, z = 5;
+
+            for (int i = 0; i < 5; i++)
+            {
+                x = y - x++ *z;
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
