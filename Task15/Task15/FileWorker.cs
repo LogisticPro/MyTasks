8dac27bcd146c266b6aab9d117d30aacf5830660
DESKTOP-JJHS10S\Влад <logisticpro3000@gmail.com>
2021-12-13 20:27:08 +0300

    Task15

---
+++ Task15/Task15/FileWorker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace Task15
+{
+    public class FileWorker
+    {
+        public List<FileInfo> FindFile(DirectoryInfo path, string fileName)
+        {
+            List<FileInfo> files = new List<FileInfo>();
+
+            files.AddRange(path.GetFiles(fileName).ToList());
+
+            var directories = path.GetDirectories();
+
+            foreach(var item in directories)
+            {
+                var a = FindFile(item, fileName);
+                files.AddRange(a);
+            }
+
+            return files;
+        }
+    }
+}
---
+++ Task15/Task15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+namespace Task15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileWorker fileWorker = new FileWorker();
+
+            var dir = new DirectoryInfo(@"D:\Ученье - свет\Practic");
+
+            var list = fileWorker.FindFile(dir, "Doc.txt");
+
+            if (list.Count != 0)
+            {
+                Console.WriteLine("It's ok!");
+
+                foreach (var item in list)
+                {                    
+                    Console.WriteLine(item.Name);
+
+                    var dest = item.DirectoryName + ".zip";
+
+                    ZipFile.CreateFromDirectory(item.DirectoryName, dest);
+                }               
+            }
+            else if(list.Count == 0)
+            {
+                throw new FileNotFoundException();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
