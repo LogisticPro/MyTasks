3250428f04dfb64c0299940f68f75391974dc695
Терещенко Влад <logisticpro3000@gmail.com>
2022-08-08 18:33:15 +0300

    SQL fix

--- Heroes3WebApp/SQLscripts/Heroes3DB.sql
+++ Heroes3WebApp/SQLscripts/Heroes3DB.sql
@@ -6,12 +6,12 @@ USE Heroes3DB
 
 GO
 
-create table Landscape(
+create table Landscapes(
 Id int not null IDENTITY(1,1),
 Name nvarchar(25),
 CONSTRAINT PK_Landscape PRIMARY KEY (Id));
 
-create table Town(
+create table Towns(
 Id int not null IDENTITY(1,1),
 LandscapeId int not null,
 Name nvarchar(25),
@@ -19,7 +19,7 @@ Discription nvarchar(100),
 Picture varbinary,
 CONSTRAINT PK_Town PRIMARY KEY (Id)); 
 
-create table Hero(
+create table Heroes(
 Id int not null IDENTITY(1,1),
 TownId int not null,
 CostId int not null,
@@ -38,7 +38,7 @@ Spellpower int not null,
 Knowledge int not null,
 CONSTRAINT PK_HeroStats PRIMARY KEY(Id));
 
-create table Spell(
+create table Spells(
 Id int not null IDENTITY(1,1),
 Name nvarchar(25),
 Discription nvarchar(100),
@@ -46,13 +46,13 @@ Picture varbinary,
 [Level] int not null,
 CONSTRAINT PK_Spell PRIMARY KEY(Id));
 
-create table Hero_Spell(
+create table Heroes_Spells(
 Id int not null IDENTITY(1,1),
 HeroId int not null,
 SpellId int not null,
 CONSTRAINT PK_Hero_Spell PRIMARY KEY(Id));
 
-create table Monster(
+create table Monsters(
 Id int not null IDENTITY(1,1),
 TownId int null,
 CostId int not null,
@@ -66,7 +66,7 @@ Discription nvarchar(100),
 Picture varbinary,
 CONSTRAINT PK_Monster PRIMARY KEY(Id));
 
-create table Building(
+create table Buildings(
 Id int not null IDENTITY(1,1),
 TownId int not null,
 CostId int not null,
@@ -75,7 +75,7 @@ Discription nvarchar(100),
 Picture varbinary,
 CONSTRAINT PK_Building PRIMARY KEY(Id));
 
-create table Cost(
+create table Costs(
 Id int not null IDENTITY(1,1),
 Gold int not null,
 Ore int null,
@@ -89,66 +89,66 @@ CONSTRAINT PK_Cost PRIMARY KEY(Id));
 GO
 ----------------------------------------------------------------
 
-ALTER TABLE [dbo].[Town]
+ALTER TABLE [dbo].[Towns]
 ADD CONSTRAINT FK_Town_LandscapeId FOREIGN KEY (LandscapeId)
-REFERENCES Landscape(Id);
+REFERENCES Landscapes(Id);
 
-ALTER TABLE [dbo].[Hero]
+ALTER TABLE [dbo].[Heroes]
 ADD CONSTRAINT FK_Hero_CostId
-FOREIGN KEY (CostId) REFERENCES Cost(Id);
+FOREIGN KEY (CostId) REFERENCES Costs(Id);
 
-ALTER TABLE [dbo].[Hero]
+ALTER TABLE [dbo].[Heroes]
 ADD CONSTRAINT FK_Hero_TownId
-FOREIGN KEY (TownId) REFERENCES Town(Id);
+FOREIGN KEY (TownId) REFERENCES Towns(Id);
 
 ALTER TABLE [dbo].[HeroStats]
 ADD CONSTRAINT FK_HeroStats_TownId
-FOREIGN KEY (TownId) REFERENCES Town(Id);
+FOREIGN KEY (TownId) REFERENCES Towns(Id);
 
-ALTER TABLE [dbo].[Hero_Spell]
+ALTER TABLE [dbo].[Heroes_Spells]
 ADD CONSTRAINT FK_HeroSpell_HeroId
-FOREIGN KEY (HeroId) REFERENCES Hero(Id);
+FOREIGN KEY (HeroId) REFERENCES Heroes(Id);
 
-ALTER TABLE [dbo].[Hero_Spell]
+ALTER TABLE [dbo].[Heroes_Spells]
 ADD CONSTRAINT FK_HeroSpell_SpellId
-FOREIGN KEY (SpellId) REFERENCES Spell(Id);
+FOREIGN KEY (SpellId) REFERENCES Spells(Id);
 
-ALTER TABLE [dbo].[Monster]
+ALTER TABLE [dbo].[Monsters]
 ADD CONSTRAINT FK_Monster_TownId
-FOREIGN KEY (TownId) REFERENCES Town(Id);
+FOREIGN KEY (TownId) REFERENCES Towns(Id);
 
-ALTER TABLE [dbo].[Monster]
+ALTER TABLE [dbo].[Monsters]
 ADD CONSTRAINT FK_Monster_CostId
-FOREIGN KEY (CostId) REFERENCES Cost(Id);
+FOREIGN KEY (CostId) REFERENCES Costs(Id);
 
-ALTER TABLE [dbo].[Building]
+ALTER TABLE [dbo].[Buildings]
 ADD CONSTRAINT FK_Building_TownId
-FOREIGN KEY (TownId) REFERENCES Town(Id);
+FOREIGN KEY (TownId) REFERENCES Towns(Id);
 
-ALTER TABLE [dbo].[Building]
+ALTER TABLE [dbo].[Buildings]
 ADD CONSTRAINT FK_Building_CostId
-FOREIGN KEY (CostId) REFERENCES Cost(Id);
+FOREIGN KEY (CostId) REFERENCES Costs(Id);
 
 GO
 -----------------------------------------------------------------
 
-INSERT INTO [dbo].[Landscape] (Name)
+INSERT INTO [dbo].[Landscapes] (Name)
 VALUES ('Dirt'), ('Sand'), ('Grass'), ('Snow'), ('Swamp'), ('Rocks'), ('Dungeon terrain'), ('Lava'), ('Highlands'), ('Wastelands'), ('Water');
 
 GO
 
-INSERT INTO [dbo].[Town] (LandscapeId, Name)
+INSERT INTO [dbo].[Towns] (LandscapeId, Name)
 VALUES ('3', 'Castle'), ('3', 'Rampart'), ('4', 'Tower'), ('8', 'Inferno'), ('1', 'Necropolis'),
        ('7', 'Dungeon'), ('6', 'Stronghold'), ('5', 'Fortress'), ('9', 'Conflux'), ('5','Cove');
 
 GO
 
-INSERT INTO [dbo].[Cost] (Gold)
+INSERT INTO [dbo].[Costs] (Gold)
 VALUES ('2500')
 
 GO
 
-INSERT INTO [dbo].[Hero] (TownId, CostId, Name, [Type])
+INSERT INTO [dbo].[Heroes] (TownId, CostId, Name, [Type])
 VALUES ('1', '1', 'Christian', '1'), ('1', '1', 'Adela', '2'),
        ('2', '1', 'Mephala', '1'), ('2', '1', 'Elleshar', '2'),
 	   ('3', '1', 'Fafner', '1'), ('3', '1', 'Solmyr', '2'),
@@ -162,7 +162,7 @@ VALUES ('1', '1', 'Christian', '1'), ('1', '1', 'Adela', '2'),
 
 GO
 
-INSERT INTO [dbo].[Spell] (Name, [Level])
+INSERT INTO [dbo].[Spells] (Name, [Level])
 VALUES ('Bless', '1'), ('Magic arrow', '1'), ('Slow', '1'),
 	   ('Weakness', '2'), ('Lightning bolt', '2'), ('Blind', '2'),
 	   ('Teleport', '3'), ('Fireball', '3'), ('Hypnotize', '3'),
